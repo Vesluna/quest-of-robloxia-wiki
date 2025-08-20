be2d6ce12b11214e769c87f510f5cc3ba90ee297
Vesluna <144409176+Vesluna@users.noreply.github.com>
2025-08-20 01:33:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function openTab(tabName) {
+    // Hide all tab content
+    var tabContents = document.getElementsByClassName("tab-content");
+    for (var i = 0; i < tabContents.length; i++) {
+        tabContents[i].classList.remove("active");
+    }
 
+    // Remove active class from all tab links
+    var tabLinks = document.getElementsByClassName("tab-link");
+    for (var i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].classList.remove("active");
+    }
+
+    // Show the selected tab content and add active class to the clicked tab
+    document.getElementById(tabName).classList.add("active");
+    event.currentTarget.classList.add("active");
+}
